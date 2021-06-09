e8f55fb645e9f20c33b8e81ee9d3de21c3435d86
Txiz <853954828@qq.com>
2021-06-09 16:17:33 +0800

    第十一次，修改部分bug，完善订单增加功能

---
+++ data/xzx-hospital-one.sql
@@ -0,0 +1,54 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Mysql
+ Source Server Type    : MySQL
+ Source Server Version : 80020
+ Source Host           : localhost:3306
+ Source Schema         : xzx-hospital-one
+
+ Target Server Type    : MySQL
+ Target Server Version : 80020
+ File Encoding         : 65001
+
+ Date: 09/06/2021 16:17:25
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for tb_order
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_order`;
+CREATE TABLE `tb_order`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uid` int NOT NULL COMMENT '用户ID',
+  `sid` int NOT NULL COMMENT '库存ID',
+  `name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '商品名称',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2054 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_order
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_stock
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_stock`;
+CREATE TABLE `tb_stock`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '名称',
+  `count` int NOT NULL COMMENT '库存',
+  `sale` int NOT NULL COMMENT '已售',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_stock
+-- ----------------------------
+INSERT INTO `tb_stock` VALUES (1, '邵迎红', 33, 11);
+
+SET FOREIGN_KEY_CHECKS = 1;
